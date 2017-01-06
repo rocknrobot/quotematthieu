487a8cc8b266df86ece2d072977145421ffede12
Andrew Torres <jandrewtorres@gmail.com>
2017-01-05 22:52:59 -0800

    skeleton

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
   $( "#update-quote" ).click(function() {
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?", function(a) {
-      console.log(a);
-      $("#title").text(a[0].title);
-      $("#content").html(a[0].content);
+      $('#title').text(a[0].title);
+      $('#content').html(a[0].content);
     });
   });
 });
