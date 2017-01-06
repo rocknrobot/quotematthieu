5a244b41d0d89920c1e71cb45e2080b61d679756
Andrew Torres <jandrewtorres@gmail.com>
2017-01-06 01:58:03 -0800

    css styling -- lightweight mobile responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
-$(document).ready(function(){
-  $( "#update-quote" ).click(function() {
+function getQuote() {
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?", function(a) {
-      $('#title').text(a[0].title);
+      $('#title').text('- ' + a[0].title);
       $('#content').html(a[0].content);
     });
-  });
+};
+
+$(document).ready(function(){
+  getQuote();
+  $( "#update-quote" ).click(getQuote);
 });
