44f9dc39d360822095701a0cdb3a4ab6b202ccdc
Andrew Torres <jandrewtorres@gmail.com>
2017-01-06 21:46:27 -0800

    added color fade in/out on newquote click with jquery colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
+var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+
+function changeColors() {
+	var color = Math.floor(Math.random() * colors.length);
+  $("#update-quote").animate({
+    backgroundColor: colors[color]
+  }, 500);
+  $(".big").animate({
+  	color: colors[color]
+  }, 500);
+}
+
 function getQuote() {
 	$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?", function(a) {
-      $('#title').text('- ' + a[0].title);
-      $('#content').html(a[0].content);
-      $("#container").fadeIn(1000);
-    });
+    $('#title').text('- ' + a[0].title);
+    $('#content').html(a[0].content);
+    $("#container").fadeIn(500);
+  });
 };
 
 function clickNewQuote() {
-	$("#container").fadeOut(1000, getQuote);
+	$("#container").fadeOut(500, getQuote);
+	changeColors();
 };
 
 $(document).ready(function(){
