e8abdf213106cd7f166819e2ed25cc75575ffecf
Andrew Torres <jandrewtorres@gmail.com>
2017-01-06 19:55:17 -0800

    added fade in/out jquery for quote container

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 function getQuote() {
-    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?", function(a) {
+	$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?", function(a) {
       $('#title').text('- ' + a[0].title);
       $('#content').html(a[0].content);
+      $("#container").fadeIn(1000);
     });
 };
 
+function clickNewQuote() {
+	$("#container").fadeOut(1000, getQuote);
+};
+
 $(document).ready(function(){
+  $("#container").hide();
   getQuote();
-  $( "#update-quote" ).click(getQuote);
+  $( "#update-quote" ).click(clickNewQuote);
 });
